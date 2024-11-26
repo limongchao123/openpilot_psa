b3e28b961e0f9d53d9c29e05a4d7535308c5cb83
elkoled <elkoled@gmail.com>
2024-11-26 11:52:31 +0100

    debug: works on linux mint 22 (wilma)

--- tools/install_ubuntu_dependencies.sh
+++ tools/install_ubuntu_dependencies.sh
@@ -94,7 +94,7 @@ function install_ubuntu_focal_requirements() {
 if [ -f "/etc/os-release" ]; then
   source /etc/os-release
   case "$VERSION_CODENAME" in
-    "jammy" | "kinetic" | "noble")
+    "jammy" | "kinetic" | "noble"| "wilma")
       install_ubuntu_lts_latest_requirements
       ;;
     "focal")
